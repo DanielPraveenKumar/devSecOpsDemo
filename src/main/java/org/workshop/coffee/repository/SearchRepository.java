1f0d843f16a0f8cda10fe1cf4cd1d1ac7474acd5
Ragavendira Rajathirumalai <ragavendira.rajathirumalai@expleogroup.com>
2023-10-08 18:21:28 +0530

    this sql injection code has been fixed

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -25,14 +25,15 @@ public class SearchRepository {
 
         //lowercase the input
         var lowerInput  = input.toLowerCase(Locale.ROOT);
-        // create a string query that matches the input to the product name or description
-        var query = "SELECT * FROM product  WHERE lower(product_name) LIKE '%" + lowerInput + "%' OR lower(description) LIKE '%" + lowerInput + "%'";
-        // create a native query from the string query and execute it
-        var result = em.createNativeQuery(query, Product.class).getResultList();
+        // create a string query parameterized that matches the input to the product name or description
+        var query = "SELECT p FROM Product p WHERE lower(p.name) LIKE :input OR lower(p.description) LIKE :input";
+        // create a query from the string
+        var typedQuery = em.createQuery(query, Product.class);
+        // set the parameter
+        typedQuery.setParameter("input", "%" + lowerInput + "%");
 
-       //test123456
-
-        return result;
+        // return the result
+        return typedQuery.getResultList();
 
     }
 
