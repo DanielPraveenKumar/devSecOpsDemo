59552589f6eb764b289c9eb3a219bc4c6c4b5cc6
Daniel <danielpk.karuppiah@expleo.com>
2023-10-09 14:41:19 +0530

    List added

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -20,21 +20,7 @@ public class SearchRepository {
 
     public List<Product> searchProduct (String input) {
 
-
-
-
-        //lowercase the input
-        var lowerInput  = input.toLowerCase(Locale.ROOT);
-        // create a string query parameterized that matches the input to the product name or description
-        var query = "SELECT p FROM Product p WHERE lower(p.name) LIKE :input OR lower(p.description) LIKE :input";
-        // create a query from the string
-        var typedQuery = em.createQuery(query, Product.class);
-        // set the parameter
-        typedQuery.setParameter("input", "%" + lowerInput + "%");
-
-        // return the results
-        return typedQuery.getResultList();
-
+        return null;
     }
 
 
