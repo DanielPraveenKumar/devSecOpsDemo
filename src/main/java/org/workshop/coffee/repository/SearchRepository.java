b1896416a9169256a97b043dd30621a90a387fdc
Daniel <danielpk.karuppiah@expleo.com>
2023-10-10 13:57:55 +0530

    List added

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -21,11 +21,13 @@ public class SearchRepository {
     public List<Product> searchProduct (String input) {
         //lower in input
         input = input.toLowerCase(Locale.ROOT);
-        // create a string query that matches the input to the product name or description
-        String query = "SELECT p FROM Product p WHERE LOWER(p.productName) LIKE '%" + input + "%' OR LOWER(p.description) LIKE '%" + input + "%'";
-        //create a native query from the string query and execute it
-        List<Product> products = em.createQuery(query, Product.class).getResultList();
-        //return the list of products
+        // create a parametrized string query that matches the input to the product name or description
+        String query = "SELECT p FROM Product p WHERE LOWER(p.productName) LIKE :input OR LOWER(p.description) LIKE :input";
+        // create a list of products that match the query
+        List<Product> products = em.createQuery(query, Product.class)
+                .setParameter("input", "%" + input + "%")
+                .getResultList();
+        // return the list of products
         return products;
 
     }
