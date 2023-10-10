b758b31c82354010d7bd7e512b77f888212d0505
Daniel <danielpk.karuppiah@expleo.com>
2023-10-10 17:38:46 +0530

    List added

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -19,21 +19,8 @@ public class SearchRepository {
     DataSource dataSource;
 
     public List<Product> searchProduct (String input) {
-        //lowercase in the input
-        input = input.toLowerCase(Locale.ROOT);
-        //create select sql statement using the input as a parameter for the query with product_name and description
-        String sql = "SELECT * FROM product WHERE LOWER(product_name) LIKE '%" + input + "%' OR LOWER(description) LIKE '%" + input + "%'";
-        // execute the native query
-        List<Product> products = em.createNativeQuery(sql, Product.class).getResultList();
-       // return the result
-        if (products.size() > 0) {
-            return products;
-        }// if no result return null
-        else {
-            return null;
-        }
 
-
+        return null;
     }
 
 
